71d4f6412ca2c7bbb256b0bb5cd191d1f2958b23
VIRAJ PATOLIYA <80443206+20IT116-VIRAJ@users.noreply.github.com>
2022-09-12 17:28:44 +0530

    Delete studentmgmtsystemNew directory

--- studentmgmtsystemNew/src/main/java/in/ac/charusat/studentmgmtsystem/repository/ResultRepository.java
+++
@@ -1,7 +0,0 @@
-package in.ac.charusat.studentmgmtsystem.repository;
-
-import in.ac.charusat.studentmgmtsystem.model.Result;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepository extends JpaRepository<Result, Integer> {
-}
